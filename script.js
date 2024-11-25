3425b092fc1152a4992221382037342ca319bd60
PugnatorStrenuus <leonardo.geodesi@gmail.com>
2024-11-25 19:29:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+// Crea una mappa centrata sulle coordinate specificate
 const map = L.map('map').setView([45.434, 12.338], 13);
 
+// Aggiungi un layer di OpenStreetMap alla mappa
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
+// Aggiungi il primo marker e il popup con l'immagine
 L.marker([45.4323, 12.3296]).addTo(map)
-  .bindPopup(`Cnaeus Numerius Fronto
+  .bindPopup(`
+    <b>Cnaeus Numerius Fronto</b>
+    <br><img src="images/Fronto_Inscription.jpg" alt="Iscrizione di Cnaeus Numerius Fronto" width="150">
   `)
   .openPopup();
 
+// Aggiungi il secondo marker e il popup con l'immagine
 L.marker([45.4375, 12.3401]).addTo(map)
-  .bindPopup(`Lucius Statius Faustus
+  .bindPopup(`
+    <b>Lucius Statius Faustus</b>
+    <br><img src="images/Faustus_Inscription.jpg" alt="Iscrizione di Lucius Statius Faustus" width="150">
   `)
   .openPopup();
