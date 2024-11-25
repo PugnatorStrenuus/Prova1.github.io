235f810f6ba723fe4a8f77dc29018c6b47d87899
PugnatorStrenuus <leonardo.geodesi@gmail.com>
2024-11-25 19:23:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 L.marker([45.4323, 12.3296]).addTo(map)
-  .bindPopup('
-             <b>Cnaeus Numerius Fronto</b>
-            <br><img src=".images/Fronto_Inscription.jpg" alt="Immagine" width="150"></br>
-  ')
+  .bindPopup(`
+    <b>Cnaeus Numerius Fronto</b>
+    <br><img src="images/Fronto_Inscription.jpg" alt="Immagine" width="150">
+  `)
   .openPopup();
 
 L.marker([45.4375, 12.3401]).addTo(map)
-  .bindPopup('
-             <b>Lucius Statius Faustus</b>
-            <br><img src=".images/Faustus_Inscription.jpg" alt="Immagine" width="150"></br>
-  ')
+  .bindPopup(`
+    <b>Lucius Statius Faustus</b>
+    <br><img src="images/Faustus_Inscription.jpg" alt="Immagine" width="150">
+  `)
   .openPopup();
