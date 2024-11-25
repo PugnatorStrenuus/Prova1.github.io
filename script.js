f84e75cef2141e7479e5a5e46ef6ceb06e90d9cd
PugnatorStrenuus <leonardo.geodesi@gmail.com>
2024-11-25 21:04:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// Crea una mappa centrata sulle coordinate specificate
 const map = L.map('map').setView([45.434, 12.338], 13);
 
-// Aggiungi un layer di OpenStreetMap alla mappa
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Aggiungi il primo marker e il popup con l'immagine
 L.marker([45.4323, 12.3296]).addTo(map)
   .bindPopup(`
     <b>Cnaeus Numerius Fronto</b>
@@ -14,7 +11,6 @@ L.marker([45.4323, 12.3296]).addTo(map)
   `)
   .openPopup();
 
-// Aggiungi il secondo marker e il popup con l'immagine
 L.marker([45.4375, 12.3401]).addTo(map)
   .bindPopup(`
     <b>Lucius Statius Faustus</b>
