6e21d4ecf46212e9a6cdd171a1e578463c5c2ef0
PugnatorStrenuus <leonardo.geodesi@gmail.com>
2024-11-25 18:47:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// Inizializza la mappa centrata su Venezia (latitudine e longitudine)
 const map = L.map('map').setView([45.434, 12.338], 13);
 
-// Aggiungi i tasselli della mappa con OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Aggiungi un marker (puntatore) su Venezia
 L.marker([45.434, 12.338]).addTo(map)
-  .bindPopup('Venezia') // Testo che appare sul marker
-  .openPopup(); // Apre automaticamente il popup
+  .bindPopup('Venezia')
+  .openPopup();
