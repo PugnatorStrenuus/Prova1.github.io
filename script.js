0f6dfa324554bd79d9f7fb292fd92ce8cc5efafd
PugnatorStrenuus <leonardo.geodesi@gmail.com>
2024-11-25 19:13:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-L.marker([45.434, 12.338]).addTo(map)
-  .bindPopup('Venezia')
+L.marker([45.4323, 12.3296]).addTo(map)
+  .bindPopup('Cnaeus Numerius Fronto')
+  .openPopup();
+
+L.marker([45.4375, 12.3401]).addTo(map)
+  .bindPopup('Lucius Statius Faustus')
   .openPopup();
